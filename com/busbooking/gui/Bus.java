c307d08b1d4e9d93b0841ea9a011acd87d39420b
mithumohan00-ship-it <mithumohan00@gmail.com>
2026-01-29 18:44:52 +0530

    Update Bus.java

--- com/busbooking/gui/Bus.java
+++ com/busbooking/gui/Bus.java
@@ -1,23 +1,22 @@
 package com.busbooking.gui;
 
 public class Bus {
-    private int id;
+    private int busNumber;
     private boolean ac;
     private int capacity;
 
-    public Bus(int id, boolean ac, int capacity) {
-        this.id = id;
+    public Bus(int busNumber, boolean ac, int capacity) {
+        this.busNumber = busNumber;
         this.ac = ac;
         this.capacity = capacity;
     }
 
-    public int getId() { return id; }
-    public boolean isAc() { return ac; }
-    public int getCapacity() { return capacity; }
-
     @Override
     public String toString() {
-        // How the ComboBox will display each Bus
-        return "Bus " + id + (ac ? " (AC)" : " (Non-AC)") + " - seats: " + capacity;
+        return "Bus " + busNumber + (ac ? " (AC)" : " (Non-AC)") + " | Capacity: " + capacity;
     }
+
+    public int getBusNumber() { return busNumber; }
+    public boolean isAc() { return ac; }
+    public int getCapacity() { return capacity; }
 }
